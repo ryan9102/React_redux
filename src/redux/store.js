92663335178e1faf9d2e6ed3905013c1292d6c46
Hualiang <ryan_li@outlook.com.au>
2021-12-27 14:11:49 +1100

    redux devtool added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
 import countReducer from './count_reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export default createStore(countReducer)
+export default createStore(countReducer,composeWithDevTools())
 
